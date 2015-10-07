54acdf9eb0661c4e7562d02e3d14407dbc7c89d2
louis <louis.chavane@gmail.com>
2015-10-07 10:41:42 +0200

    Initial Commit

---
+++ Cargo/Cargo.h
@@ -0,0 +1,13 @@
+//
+//  Cargo.h
+//  Cargo
+//
+//  Created by louis chavane on 07/10/15.
+//  Copyright © 2015 55 SAS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cargo : NSObject
+
+@end
---
+++ Cargo/Cargo.m
@@ -0,0 +1,13 @@
+//
+//  Cargo.m
+//  Cargo
+//
+//  Created by louis chavane on 07/10/15.
+//  Copyright © 2015 55 SAS. All rights reserved.
+//
+
+#import "Cargo.h"
+
+@implementation Cargo
+
+@end
